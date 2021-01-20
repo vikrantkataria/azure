d8bfb6e3cbdd6dff96c5e7ff066ae937b6e28373
Vikrant Kataria <vikrantkataria289@gmail.com>
2021-01-20 14:17:42 +0530

    update

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
 
-/*
+
 provider "azurerm" {
   version = "=2.2.0"
   features {}
+  /*
   subscription_id = ${{ secrets.SUBSCRIPTION_ID }}
   tenant_id       = ${ secrets.TENANT_ID }
   client_id       = ${ secrets.CLIENT_ID }
-  client_secret   = ${ secrets.CLIENT_SECRET }
+  client_secret   = ${ secrets.CLIENT_SECRET }*/
 }
-*/
