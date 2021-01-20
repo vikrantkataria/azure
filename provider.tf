e910b9683aac16ddaa9c8d52808a798cf211c30b
vikrantkataria <41620872+vikrantkataria@users.noreply.github.com>
2021-01-20 12:25:53 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  version = "=2.2.0"
+  features {}
+  subscription_id = "7ae7464d-8b30-4422-bc14-b1a66910a36f"
+  tenant_id       = "b9efe91a-483e-42fd-a529-7eb2da91d86a"
+  client_id       = "ab0dd0a6-b7cc-4ff5-9117-c370a65db562"
+  client_secret   = "l--z9AI5mip6_okVKrG4GRn48.THHK~q.T"
+} 
